d353fc6c4fa06f06d391636b5c6853a9bcc6f499
thehaystacker <thehaystacker.in@gmail.com>
2019-11-18 00:39:31 +0530

    Using yargs package

--- filesystem-and-cmd/app.js
+++ filesystem-and-cmd/app.js
@@ -1,9 +1,47 @@
+const yargs = require("yargs");
 const { getNotes } = require("./utils");
 
-const command = process.argv[2];
+yargs.command({
+  command: "add",
+  describe: "Adds a new note to the database",
+  handler: function() {
+    console.log(" [Analytics] Sending data to server::ADD_NOTE");
+  }
+});
 
-console.log(" [process.argv] ", process.argv);
+yargs.command({
+  command: "delete",
+  describe: "Deletes a note from database using note ID",
+  handler: function() {
+    console.log(" [Analytics] Sending data to server::DELETE_NOTE ");
+  }
+});
 
-if (command === "add") {
-  console.log("Adding notes");
+const [command] = yargs.argv._;
+
+switch (command) {
+  case "add":
+    console.log("[Adding note]");
+
+    break;
+
+  case "delete":
+    console.log("[Deleting note]");
+
+    break;
+
+  case "read":
+    console.log("[Reading note]");
+
+    break;
+
+  case "list":
+    console.log("[Listing all notes]");
+
+    break;
+
+  default:
+    console.log("[Command not accepted]");
+
+    break;
 }
