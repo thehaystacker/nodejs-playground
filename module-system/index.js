e637b5f1972fb660cb1635fe22fa1a1bfab0d00c
thehaystacker <thehaystacker.in@gmail.com>
2019-11-17 23:49:38 +0530

    working with modules

--- module-system/index.js
+++ module-system/index.js
@@ -1,1 +1,21 @@
 const fs = require("fs");
+const { name, age } = require("./utils");
+
+console.log(name);
+console.log(age);
+
+createFile("\nSecond Sentence");
+
+function createFile(content, begin) {
+  const filePath = "./notes.txt";
+
+  if (begin) {
+    fs.truncate(filePath, 0, function() {
+      console.log("File Cleared");
+
+      fs.appendFileSync(filePath, content);
+    });
+  } else {
+    fs.appendFileSync(filePath, content);
+  }
+}
---
+++ module-system/utils.js
@@ -0,0 +1,11 @@
+// module.exports.name = 'Sangeeth';
+// module.exports.age = '24';
+
+module.exports = {
+  name: 'Sangeeth',
+  age: '24'
+}
+
+module.exports = {
+  name: 'Sarath'
+}
