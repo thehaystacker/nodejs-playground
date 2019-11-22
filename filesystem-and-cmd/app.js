c34dac819e7d6da0ae00e7124f8537a0251874fd
thehaystacker <thehaystacker.in@gmail.com>
2019-11-23 00:54:26 +0530

    Function inside function

--- filesystem-and-cmd/app.js
+++ filesystem-and-cmd/app.js
@@ -1,74 +1,29 @@
-const fs = require("fs");
-const yargs = require("yargs");
-const { getNotes } = require("./utils");
-
-yargs.command({
-  command: "add",
-  describe: "Adds a new note to the database",
-  builder: {
-    title: {
-      describe: "Title of the note",
-      demandOption: true,
-      type: "string"
+// ORIGINAL OBJECT
+var accessor = {
+  sortable: {
+    get: function() {
+      return typeof this.getAttribute("name") != undefined;
     }
   },
-  handler: function(argv) {
-    console.log(" [Analytics] Sending data to server::ADD_NOTE", argv);
+  draggable: {
+    get: function() {
+      return typeof this.getAttribute("id") != undefined;
+    }
   }
-});
+};
 
-yargs.command({
-  command: "delete",
-  describe: "Deletes a note from database using note ID",
-  handler: function() {
-    console.log(" [Analytics] Sending data to server::DELETE_NOTE ");
+// CAN BE CONVERTED INTO
+var getAttributeCB = function(attr) {
+  return function() {
+    return typeof this.getAttribute(attr) != undefined;
+  };
+};
+
+var accessor = {
+  sortable: {
+    get: getAttributeCB("name");
+  },
+  draggable: {
+    get: getAttributeCB("id")
   }
-});
-
-yargs.parse();
-
-console.log("[yargs.argv]", yargs.argv);
-
-const [command] = yargs.argv._;
-const { title } = yargs.argv;
-
-console.log("[title]", title);
-
-let notes = [];
-let note = { title };
-notes.push(note);
-
-switch (command) {
-  case "add":
-    let notes = fs.readFile("./notes.json", null, function(err, notes) {
-      console.log(" [err] ", err);
-
-      if (err) {
-        fs.writeFile("./notes.json", JSON.stringify([{title}]), function() {});
-      } else {
-        console.log(" [notes] ", JSON.parse(notes.toString()));
-      }
-    });
-
-    break;
-
-  case "delete":
-    console.log("[Deleting note]");
-
-    break;
-
-  case "read":
-    console.log("[Reading note]");
-
-    break;
-
-  case "list":
-    console.log("[Listing all notes]");
-
-    break;
-
-  default:
-    console.log("[Command not accepted]");
-
-    break;
-}
+};
