8303a0f13f0e1c2c6eca6acfccf28cfcd2f71036
thehaystacker <thehaystacker.in@gmail.com>
2019-11-29 01:07:30 +0530

    Using Pug

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+const port = "4001";
+
+const pathPublicDir = path.join(__dirname, "../public");
+const pathViewsDir = path.join(__dirname, "../views/pages");
+
+app.set("view engine", "pug");
+app.set('views', pathViewsDir)
+app.use(express.static(pathPublicDir));
+
+app.get("/", (request, response) => {
+  response.render("index", { title: "The Title", content: "Hello World!", something: '../dfd' });
+});
+
+app.get("/help", (request, response) => {
+  response.render("help", { title: "Help", content: "Help Page" });
+});
+
+app.get("/contact", (request, response) => {
+  response.render("index", { title: "Contact", content: "Contact Page" });
+});
+
+app.get("*", (request, response) => {
+  response.render("page404", {
+    title: "Page Not Found",
+    content: "404 :: Page Not Found"
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server is running on Port: " + port);
+});
