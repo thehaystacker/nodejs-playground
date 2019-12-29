ae4923b024fa097eb4d28933be5d72f7a6fe696e
thehaystacker <thehaystacker.in@gmail.com>
2019-12-29 12:39:50 +0530

    insert one

--- mongodb/src/index.js
+++ mongodb/src/index.js
@@ -1,12 +1,29 @@
-const {MongoClient} = require('mongodb');
+const { MongoClient } = (mongodb = require("mongodb"));
 
-const connectionUrl = 'mongodb://127.0.0.1:12017';
-const dbName = 'db_users';
+const connectionUrl = "mongodb://127.0.0.1:27017";
+const db_users = "db_users";
+const db_tasks = "db_tasks";
 
-MongoClient.connect(connectionUrl + '/' + dbName, { useNewUrlParser: true }, function (error, status) {
-  if (error) {
-    return console.log('[connecting to database failed]');
+MongoClient.connect(
+  connectionUrl,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      console.log("[connecting to database failed]", error.message);
+      return;
+    }
+
+    const dbUsers = client.db(db_users);
+    const dbTasks = client.db(db_tasks);
+
+    dbUsers.collection("users").insertOne({
+      name: "Sangeeth",
+      age: 20
+    });
+
+    dbTasks.collection("tasks").insertOne({
+      title: "Task 1",
+      description: "Do it before 1 PM"
+    });
   }
-
-  console.log('[connection successfull]');
-})
+);
