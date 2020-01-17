d0200d475acf56134f57a01fd9a01c7d16b70e8f
thehaystacker <thehaystacker.in@gmail.com>
2020-01-17 23:03:47 +0530

    comments added

--- rest-api/src/index.js
+++ rest-api/src/index.js
@@ -7,6 +7,8 @@ const port = process.env.port || 3010;
 app.use(express.json());
 
 
+// USING THE EXPRESS JS ROUTES TO CREATE THE ROUTES
+
 app.listen(port, () => {
   console.log("[App listening on port : " + port + "]");
 });
